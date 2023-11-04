121e07c4765ec1741e4ee370f3184dfa0132d54f
NeuronaBens <71675911+NeuronaBens@users.noreply.github.com>
2023-11-03 21:32:07 -0500

    fine emoji

--- puzzles/find-emoji.js
+++ puzzles/find-emoji.js
@@ -1,5 +1,8 @@
 //300px - 22 chars -> 13.45px per char
 
+var score = 0;
+var time = 0;
+
 function createEmojiGrid(columns, rows, emoji1, emoji2, charsize) {
   const x = document.getElementById("ec");
   const px = String(columns * charsize) + "px";
@@ -9,25 +12,51 @@ function createEmojiGrid(columns, rows, emoji1, emoji2, charsize) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       if (i * columns + j == rand) {
-        text += emoji2;
+        text += "<span id='icon'>" + emoji2 + "</span>";
       } else {
-        text += emoji1;
+        text += "<span>" + emoji1 + "</span>";
       }
     }
   }
-  x.innerText = text;
+  x.innerHTML = text;
+}
+
+
+function setUp() {
+  // Get the selected emoji pair from the dropdown
+  var selectedEmoji = document.getElementById("emojiSelect").value.split(",");
+
+  // Get the values for rows and columns
+  var rows = parseInt(document.getElementById("rows").value);
+  var columns = parseInt(document.getElementById("columns").value);
+
+  // Call the createEmojiGrid function with the selected emoji values
+  createEmojiGrid(columns, rows, selectedEmoji[0], selectedEmoji[1], 13.45);
+
+  var distinctIcon = document.getElementById("icon");
+  distinctIcon.addEventListener("click", function () {
+    distinctIcon.className = "green";
+  });
+  distinctIcon.addEventListener("dblclick", (event) => {
+      setUp();
+      score += 1;
+      document.getElementById("score").innerText = "your score is: " + score.toString();
+  });
+}
+
+
+function increaseTimer() {
+  time += 1;
+  document.getElementById("time").innerText = "Time elapsed: " + time.toString();
+  if(time == 5) {
+    const x = document.getElementById("ec");
+    x.innerHTML = "<div style='font-size: 2em;'>😞</div>"
+  }
 }
 
 document
   .getElementById("generateButton")
   .addEventListener("click", function () {
-    // Get the selected emoji pair from the dropdown
-    var selectedEmoji = document.getElementById("emojiSelect").value.split(",");
-
-    // Get the values for rows and columns
-    var rows = parseInt(document.getElementById("rows").value);
-    var columns = parseInt(document.getElementById("columns").value);
-
-    // Call the createEmojiGrid function with the selected emoji values
-    createEmojiGrid(columns, rows, selectedEmoji[0], selectedEmoji[1], 13.45);
+    setUp();
+    setInterval(increaseTimer, 1000);
   });
